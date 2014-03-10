a2601a9c91e918549eb6e9a069d74511486dce12
Deshi Rahim <deshi@deshiknaves.com>
2014-03-10 21:07:47 +1100

    Added the ability to point input to a directory and output to a file. This will entangle all the files in that directory and combine their output.

--- lib/guard/entangle/runner.rb
+++ lib/guard/entangle/runner.rb
@@ -32,10 +32,15 @@ module Guard
         else
           # We need to check to see if the input is a
           # directory, else only check that file
-          if File.directory?(@options[:input])
-            compile_files(files)
+          if File.directory?(options[:input])
+            # Check if the output is one file
+            if output_dir?
+              compile_files(files)
+            else
+              compile_all(files)
+            end
           else
-            compile(@options[:input])
+            compile(options[:input])
           end
         end
       end
@@ -51,7 +56,11 @@ module Guard
           options = @options.merge(@options[:run_all]).freeze
           return if paths.empty?
           ::Guard::UI.info(options[:message], reset: true)
-          run_paths(paths, options)
+          if output_dir?
+            run_paths(paths)
+          else
+            compile_all(paths)
+          end
         else
           compile(paths)
         end
@@ -66,13 +75,13 @@ module Guard
       # @param  [hash]  options The options
       # @return [void]
       #
-      def run_paths(paths, options)
+      def run_paths(paths)
         if paths.kind_of?(Array)
           paths.each do |path|
-            process_dir(path, options)
+            process_dir(path)
           end
         elsif paths.kind_of?(String)
-          process_dir(paths, options)
+          process_dir(paths)
         else
           ::Guard::UI.info "Paths in configuration are incorrect"
         end
@@ -85,23 +94,42 @@ module Guard
       # @param  [hash]  options The options
       # @return [void]
       #
-      def process_dir(paths, options)
+      def process_dir(paths, only_compile = false)
         return false unless File.directory?(paths)
         skip = %w[. ..];
 
         cwd = Dir.pwd
         path = "#{cwd}/#{paths}"
+        compiled = ''
 
         entries = Dir.entries(path)
         entries.each do |file|
           # Skip the dot files and the partials
           if not skip.include?(file) and not partial?(file)
             if File.directory?("#{path}/#{file}")
-              process_dir("#{paths}/#{file}", options)
+              contents = process_dir("#{paths}/#{file}", only_compile)
             else
-              compile("#{path}/#{file}")
+              contents = compile("#{path}/#{file}", only_compile)
             end
+            compiled << contents if only_compile
           end
+        end
+        if (only_compile)
+          return compiled
+        end
+      end
+
+      # Compile all files and save then in the same file
+      #
+      # @param [string] path The input path
+      # @return [void]
+      #
+      def compile_all(path)
+        compiled = process_dir(path, true)
+        saved = @writer.output(compiled, options[:output])
+        if saved
+          message = "Successfully compiled and saved #{ options[:output] }"
+          @formatter.success(message)
         end
       end
 
@@ -123,8 +151,12 @@ module Guard
       # @param [string] file The file to compile
       # @return [string] The saved file
       #
-      def compile(file)
+      def compile(file, only_compile = false)
         contents = @entangler.convert(file)
+
+        # If we are only compiling
+        return contents if only_compile
+
         # save the contents to a file
         if contents
           saved = @writer.output(contents, file)
@@ -146,6 +178,10 @@ module Guard
       #
       def partial?(path)
         File.basename(path).start_with? '_'
+      end
+
+      def output_dir?
+        File.extname(options[:output]).empty?
       end
     end
   end
